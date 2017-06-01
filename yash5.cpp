554088b71b5dfb7bf90d979906e7072546fc6d29
tyashwant-mirp <tyashwant-mirp@mit.edu>
2017-06-02 03:46:56 +0530

    fixed files to push

---
+++ yash5.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int n,a;
+cout<<" Enter a number"<<endl;
+cin>>n;
+for(a=0;a<=n;a+=2);
+
+cout<<" Sum of even numbers upto n:"<<a<<endl;
+return 0;
+}
