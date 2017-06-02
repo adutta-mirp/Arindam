0b270c4edd4e44b9de61bf0cf0461af13e192919
Your Name <you@example.com>
2017-06-02 12:17:00 +0530

    Added abc.cpp

---
+++ abc.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int n, a, b, c, d;
+ int n;
+cout<<"enter a number: ";
+cin>>n;
+int a;
+cout<<"the last digit"<< a=n%10;
+cin>>a;
+int b;
+cout<<"the last digit"<< b=n%10;
+cin>>b;
+int c;
+cout<<"the last digit"<< c=n%10;
+cin>>c
+int d;
+cout<<"the sum of last three digit"<< d = a+b=c<<endl;
+return 0;
+}
+
+
+
+
+
